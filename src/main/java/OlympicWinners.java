1660ad4f8fbd72a5744b7d3063be07e989cd144b
PedroHombre <radekgornackiv2@gmail.com>
2020-12-19 17:14:24 +0100

    odnajdywanie kokretnych danych na stronie

--- src/main/java/OlympicWinners.java
+++ src/main/java/OlympicWinners.java
@@ -12,14 +12,19 @@ public class OlympicWinners {
         Document doc;
         try {
             doc = Jsoup.connect("http://www.theolympicdatabase.nl/olympic/sporters").get();
-            System.out.println(doc.html());
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
         Elements rows = doc.getElementsByClass("datagrid_header_table").first().getElementsByTag("tr");
         for (Element r : rows) {
-            System.out.println(r.html());
+            Elements data = r.getElementsByTag("td");
+            System.out.println(data.get(1).getElementsByTag("a").text());
+            System.out.println(data.get(2).getElementsByTag("a").attr("title"));
+            System.out.println(data.get(3).getElementsByTag("b").text());
+            System.out.println(data.get(4).getElementsByTag("b").text());
+            System.out.println(data.get(5).getElementsByTag("b").text());
+            System.out.println("\n\n\n");
         }
     }
 
